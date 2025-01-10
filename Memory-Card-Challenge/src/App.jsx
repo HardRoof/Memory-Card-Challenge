08c17522348c27c72b6f80d477db980215c23685
HardRoof <92646158+HardRoof@users.noreply.github.com>
2025-01-10 22:45:54 +0000

    Working on App logic

--- Memory-Card-Challenge/src/App.jsx
+++ Memory-Card-Challenge/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Scoreboard from "./components/Scoreboard";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return;
+  <>
+    <header>
+      <div>Pokémon Memory Card Challenge</div>{" "}
+      <p>
+        Catch points by clicking on each Pokémon, but beware: you can only catch
+        each one once!
+      </p>
+      <Scoreboard />
+    </header>
+    <Card />
+    <footer>
+      Pokémon Memory Card Challenge
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/900px-Octicons-mark-github.svg.png?20180806170715"
+        alt="Github"
+        id="github"
+        onClick={() => window.open("https://github.com/HardRoof", "_blank")}
+      />
+    </footer>
+  </>;
 }
 
 export default App;
