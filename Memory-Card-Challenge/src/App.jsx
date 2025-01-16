c3cba7318ffd5b52fab94e9f31d694dffc1fd7e3
HardRoof <92646158+HardRoof@users.noreply.github.com>
2025-01-16 23:01:56 +0000

    Updated Fetch logic

--- Memory-Card-Challenge/src/App.jsx
+++ Memory-Card-Challenge/src/App.jsx
@@ -4,52 +4,92 @@ import Scoreboard from "./components/Scoreboard";
 import "./index.css";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [cardsDisplayed, setCardsDisplayed] = useState([]);
-  const [caught, setCaught] = useState([]);
-  const [score, setScore] = useState(0);
-  const [bestScore, setbestScore] = useState(0);
+  const [primaryData, setPrimaryData] = useState(null);
+  const [caught, setCaught] = useState(new Set());
+  const [cardsDisplayed, setCardsDisplayed] = useState(new Set());
+  const [pokemonData, setPokemonData] = useState([]);
+  // const [score, setScore] = useState(0);
+  // const [bestScore, setbestScore] = useState(0);
 
-  const fetchCard = async () => {
+  useEffect(() => {
+    fetchPrimaryData();
+  }, []);
+
+  const fetchPrimaryData = async () => {
     try {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0"
+        "https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0"
       );
-      const dataResult = await response.json();
-      setData(dataResult);
-      console.log(dataResult);
+      const primaryDataTemp = await response.json();
+      setPrimaryData(primaryDataTemp.results);
+      while (cardsDisplayed <= 9) {
+        fetchPokemon();
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => {
-    fetchCard();
-  }, []);
+  const fetchPokemon = async () => {
+    let randomNumber = generateUniquePokeId();
+    try {
+      const response = await fetch(primaryData[randomNumber].url);
+      const pokemonDataTemp = await response.json();
+      // setPokemonData((prevArr) => [...prevArr, pokemonDataTemp]);
+      setCardsDisplayed((prevArr) => [...prevArr, pokemonDataTemp.id]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const handleCardClick = () => {};
+  const generateUniquePokeId = () => {
+    // Generates an array with all possible poke IDs
+    const totalPokemons = 1025;
+    const allNumbersArr = Array.from(
+      { length: totalPokemons },
+      (_, i) => i + 1
+    );
+    const unavailableNumbers = new Set([...cardsDisplayed, ...caught]);
 
-  return;
-  <>
-    <header>
-      <div>Pokémon Memory Card Challenge</div>{" "}
-      <p>
-        Catch points by clicking on each Pokémon, but beware: you can only catch
-        each one once!
-      </p>
-      <Scoreboard />
-    </header>
-    <Card />
-    <footer>
-      Pokémon Memory Card Challenge
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/900px-Octicons-mark-github.svg.png?20180806170715"
-        alt="Github"
-        id="github"
-        onClick={() => window.open("https://github.com/HardRoof", "_blank")}
-      />
-    </footer>
-  </>;
+    // Filter out existing numbers
+    const availableNumbers = allNumbersArr.filter(
+      (num) => !unavailableNumbers.has(num)
+    );
+
+    // Randomly select a number from the remaining numbers
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+    return availableNumbers[randomIndex];
+  };
+
+  // const handleCardClick = () => {};
+  const handleClick = () => {
+    console.log(primaryData);
+  };
+
+  return (
+    <>
+      <header>
+        <div>Pokémon Memory Card Challenge</div>{" "}
+        <p>
+          Catch points by clicking on each Pokémon, but beware: you can only
+          catch each one once!
+        </p>
+        <Scoreboard />
+      </header>
+      {primaryData && <Card primaryData={primaryData} caught={caught} />}
+      <footer>
+        Pokémon Memory Card Challenge
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/900px-Octicons-mark-github.svg.png?20180806170715"
+          alt="Github"
+          id="github"
+          style={{ height: "30px" }}
+          onClick={() => window.open("https://github.com/HardRoof", "_blank")}
+        />
+      </footer>
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
 }
 
 export default App;
--- Memory-Card-Challenge/src/components/Card.jsx
+++ Memory-Card-Challenge/src/components/Card.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Card = () => {
-  const cards = Array.from({ length: 9 }, (_, index) => (
+const Card = ({ data, caught }) => {
+  const amountOfCards = 9;
+  const cards = Array.from({ length: amountOfCards }, (_, index) => (
     <div key={index} className="card">
-      {/* Card content goes here */}
+      {/* <img src={`${data[index].url}sprites/front_default`} alt={data.name} /> */}
+      {/* <p>{data[2].name}</p> */}
     </div>
   ));
   return <main>{cards}</main>;
