b45190b3b491bfa625a0ca1d618689a046c08378
HardRoof <92646158+HardRoof@users.noreply.github.com>
2025-02-07 22:05:38 +0000

    Updated classNames

--- Memory-Card-Challenge/src/components/Scoreboard.jsx
+++ Memory-Card-Challenge/src/components/Scoreboard.jsx
@@ -1,12 +1,12 @@
 const Scoreboard = ({ score, bestScore }) => {
   return (
-    <div className="headerScoreboard">
-      <div className="score">
-        <p>Score:</p>
+    <div className="header_scoreboard">
+      <div className="scoreboard_score">
+        <p>Score</p>
         <span>{score}</span>
       </div>
-      <div className="bestScore">
-        <p>Best score:</p>
+      <div className="scoreboard_bestScore">
+        <p>Best score</p>
         <span>{bestScore}</span>
       </div>
     </div>
